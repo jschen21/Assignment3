9ae811f02c8079e91fe6541c27b65bfb6c8cde34
jchen21 <jonathan.s.chen3@gmail.com>
2017-10-26 15:07:43 -0400

    Worked on BasicReceipt

--- src/edu/towson/cosc/cosc436/jchen21/assignment3/BasicReceipt.java
+++ src/edu/towson/cosc/cosc436/jchen21/assignment3/BasicReceipt.java
@@ -7,18 +7,28 @@ public class BasicReceipt implements Receipt {
     private String storeInfo; // store number, store address, phone number
     private String stateCode; // MD, DE, CA or MA
     private PurchasedItems items;
-    private Date date;
+    private ReceiptDate date;
     private TaxComputation tc;
     public BasicReceipt(PurchasedItems items) {
         this.items = items;
     }
+    public void setDate(ReceiptDate date) {
+        this.date = date;
+    }
     public void setTaxComputation(TaxComputation tc) {
         this.tc = tc;
     }
-    public void setDate(String date) {
-        this.date = date;
+    public void setStoreInfo(String storeInfo){
+        this.storeInfo = storeInfo;
     }
+    public void setStateCode(String stateCode){
+        this.stateCode = stateCode;
+    }
+
     public void prtReceipt() {
-// to implement
+        double subtotal = items.getTotalCost();
+        double tax = tc.computeTax(items, date);
+        double total = subtotal + tax;
+
     }
 }
