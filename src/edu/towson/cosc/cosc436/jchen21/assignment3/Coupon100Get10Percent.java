0ffe869e596c0983529f99b8b2bf46ef873da910
jchen21 <jonathan.s.chen3@gmail.com>
2017-10-26 15:13:59 -0400

    Worked on Coupon100Get10Percent

--- src/edu/towson/cosc/cosc436/jchen21/assignment3/Coupon100Get10Percent.java
+++ src/edu/towson/cosc/cosc436/jchen21/assignment3/Coupon100Get10Percent.java
@@ -3,5 +3,17 @@ package edu.towson.cosc.cosc436.jchen21.assignment3;
 /**
  * Created by jchen21 on 10/19/2017.
  */
-public class Coupon100Get10Percent implements AddOn, Coupon { // similar to rebate class
+public class Coupon100Get10Percent implements AddOn, Coupon {
+    @Override
+    public boolean applies(PurchasedItems items) {
+        if(items.getTotalCost() >= 100){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getLines() {
+        return "Thanks for spending over $100! Here is a coupon for 10% off your next purchase.";
+    }
 }
